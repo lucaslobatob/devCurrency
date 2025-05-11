8b3b4d3d189b6b08eb2b58a56a9f16885b11e384
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-05-11 20:31:16 -0300

    Create form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -49,7 +49,7 @@ export function Home() {
                             2B
                         </td>
 
-                        <td className={styles.tdLabel} data-label="Mudança 24h">
+                        <td className={styles.tdProfit} data-label="Mudança 24h">
                             <span>1.20</span>
                         </td>
                     </tr>
