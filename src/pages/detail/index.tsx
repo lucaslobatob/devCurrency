6503b43d6878fc8a00edcf47ffdbb28ed3898a61
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-05-14 19:15:08 -0300

    Create detail coin page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import type { CoinProps } from '../home';
+import styles from './detail.module.css';
 
 interface ResponseData {
     data: CoinProps;
@@ -17,6 +18,7 @@ export function Detail() {
     const navigate = useNavigate();
 
     const [coin, setCoin] = useState<CoinProps>()
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function getCoin() {
@@ -48,6 +50,7 @@ export function Detail() {
                         }
 
                         setCoin(resultData);
+                        setLoading(false);
 
                     })
             } catch (error) {
@@ -58,9 +61,31 @@ export function Detail() {
         getCoin();
     }, [cripto]);
 
+    if (loading || !coin) {
+        return (
+            <div className={styles.container}>
+                <h4 className={styles.center}>Carregando...</h4>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Página Detalhe da {cripto}</h1>
+        <div className={styles.container}>
+            <h1 className={styles.center}>{coin?.name}</h1>
+            <h1 className={styles.center}>{coin?.symbol}</h1>
+
+            <section className={styles.content}>
+                <img
+                    src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`}
+                    alt={coin?.name}
+                    className={styles.logo}
+                />
+                <h1>{coin?.name} | {coin?.symbol}</h1>
+                <p><strong>Preço: </strong>{coin?.formatedPrice}</p>
+                <a><strong>Mercado: </strong>{coin?.formatedMarket}</a>
+                <a><strong>Volume: </strong>{coin?.formatedVolume}</a>
+                <a><strong>Mudança 24h: </strong><span className={Number(coin?.changePercent24Hr) > 0 ? styles.profit : styles.loss}>{Number(coin?.changePercent24Hr).toFixed(3)}%</span></a>
+            </section>
         </div>
     )
 }
